ef26796b4228f7579b7977e0aaf7f7d332b79e5e
deepcourse <deepcourse@163.com>
2018-11-24 06:25:18 +0000

    fix some comment

--- ch3_hough_lane/detect_lane.py
+++ ch3_hough_lane/detect_lane.py
@@ -5,19 +5,20 @@ import numpy as np
 #利用霍夫变换在图像中寻找直线，从而实现车道线检测
 if __name__ == "__main__":
     image = cv2.imread("lane.jpg")
-    image = image[550:, :, :] #取感兴趣区域
+    image = image[550:, :, :] #取感兴趣区域, 将天空部分去掉
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #转为灰度图
-    binary = (gray > 150).astype(np.uint8) * 255   #通过阈值转为二值图像
+    binary = (gray > 150).astype(np.uint8) * 255   #通过阈值150转为二值图像
     cv2.imshow("binary", binary)
     
-    edges = cv2.Canny(binary, 350, 200)            #边缘检测
+    edges = cv2.Canny(binary, 350, 200)            #Canny算子边缘检测
     cv2.imshow("edge", edges)
-    lines = cv2.HoughLines(edges, 1, np.pi/180, 200) #霍夫变换
+    lines = cv2.HoughLines(edges, 1, np.pi/90, 200) #霍夫变换
     lines = lines[:, 0, :]
   
     for r, theta in lines:
-        k = -1 / np.tan(theta)  #极坐标变为直角坐标y = kx + b
+        k = -1 / np.tan(theta)  #Hesse法线式r = xsin(θ) + ycos(θ)变为直角坐标y = kx + b
         b = r / np.sin(theta)
+        #根据y = kx + b，取两个点(0, b), (w, k * w + b) 用来画线
         x1 = 0                  #x = 0是图像最左边的点
         y1 = int(k * x1 + b)
         x2 = image.shape[1] - 1    #image.shape = h, w, c所以x2是图像宽度减一,也就是最右边的点
